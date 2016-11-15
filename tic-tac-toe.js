deafed4f857ad40fe7fc81777b42f75b11bd08ad
Natalia Garcia <www.nataliagarcia@gmail.com>
2016-11-15 14:02:00 -0800

    one

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,9 +4,18 @@ var namespace = "http://www.w3.org/2000/svg"
 function firstcircle() {
    var Shape = document.createElementNS(namespace, "circle")
    var canvas = document.getElementById("game-board")
-   Shape.setAttribute("cx", 100)
+   Shape.setAttribute("cx", 40)
    Shape.setAttribute("cy", 50)
    Shape.setAttribute("r", 30)
    Shape.setAttribute("fill", "green")
    canvas.appendChild(Shape)
+}
+
+function secondcircle() {
+  var Shapetwo = document.createElementNS(namespace, "circle")
+  var canvas = document.getElementId("topmiddle")
+  Shapetwo.setAttribute("cx", 40)
+  Shapetwo.setAttribute("cy", 50)
+  Shapetwo.setAttribute("r", "green")
+  canvas.appendChild(Shapetwo)
 }
