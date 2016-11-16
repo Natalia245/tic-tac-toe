17907152df26e2ba1acc24a4854c830c33db68eb
Natalia Garcia <www.nataliagarcia@gmail.com>
2016-11-16 14:01:52 -0800

    second

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,7 +4,7 @@ var namespace = "http://www.w3.org/2000/svg"
 function firstcircle() {
    var Shape = document.createElementNS(namespace, "circle")
    var canvas = document.getElementById("game-board")
-   Shape.setAttribute("cx", 40)
+   Shape.setAttribute("cx", 45)
    Shape.setAttribute("cy", 50)
    Shape.setAttribute("r", 30)
    Shape.setAttribute("fill", "green")
@@ -13,9 +13,24 @@ function firstcircle() {
 
 function secondcircle() {
   var Shapetwo = document.createElementNS(namespace, "circle")
-  var canvas = document.getElementId("topmiddle")
-  Shapetwo.setAttribute("cx", 40)
+  var canvas = document.getElementById("game-board")
+  Shapetwo.setAttribute("cx", 136)
   Shapetwo.setAttribute("cy", 50)
-  Shapetwo.setAttribute("r", "green")
+  Shapetwo.setAttribute("r", 30)
+  Shapetwo.setAttribute("fill", "green")
   canvas.appendChild(Shapetwo)
+}
+
+function thirdcircle() {
+  var Shapethree = document.createElementNS(namespace, "circle")
+  var canvas = document.getElementById("game-board")
+  Shapethree.setAttribute("cx", 225)
+  Shapethree.setAttribute("cy", 50)
+  Shapethree.setAttribute("r", 30)
+  Shapethree.setAttribute("fill", "green")
+  canvas.appendChild(Shapethree)
+}
+
+function fourthcircle() {
+
 }
