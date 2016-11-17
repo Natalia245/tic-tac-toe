3115e72069f84412b96e2af2ca2e9a7be0324d86
Natalia Garcia <www.nataliagarcia@gmail.com>
2016-11-17 14:02:35 -0800

    third

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -32,5 +32,41 @@ function thirdcircle() {
 }
 
 function fourthcircle() {
+var Shapefour = document.createElementNS(namespace, "circle")
+var canvas = document.getElementById("game-board")
+Shapefour.setAttribute("cx", 45)
+Shapefour.setAttribute("cy", 140)
+Shapefour.setAttribute("r", 30)
+Shapefour.setAttribute("fill", "green")
+canvas.appendChild(Shapefour)
+}
 
+function fifthcircle() {
+var Shapefive = document.createElementNS(namespace, "circle")
+var canvas = document.getElementById("game-board")
+Shapefive.setAttribute("cx", 45)
+Shapefive.setAttribute("cy", 225)
+Shapefive.setAttribute("r", 30)
+Shapefive.setAttribute("fill", "green")
+canvas.appendChild(Shapefive)
+}
+
+function sixthcircle() {
+var Shapesix = document.createElementNS(namespace, "circle")
+var canvas = document.getElementById("game-board")
+Shapesix.setAttribute("cx", 135)
+Shapesix.setAttribute("cy", 140)
+Shapesix.setAttribute("r", 30)
+Shapesix.setAttribute("fill", "green")
+canvas.appendChild(Shapesix)
+}
+
+function seventhcircle() {
+var Shapeseven = document.createElementNS(namespace, "circle")
+var canvas = document.getElementById("game-board")
+Shapeseven.setAttribute("cx", 225)
+Shapeseven.setAttribute("cy", 225)
+Shapeseven.setAttribute("r", 30)
+Shapeseven.setAttribute("fill", "green")
+canvas.appendChild(Shapeseven)
 }
